093b06e6f40ca357b3a67bfb1f30abf74b96e486
nabilhussein <55296823+nabilhussein@users.noreply.github.com>
2023-05-03 13:19:40 -0400

    third commit

--- src/main/java/com/accounting/app/AccountingLedgerAppHome.java
+++ src/main/java/com/accounting/app/AccountingLedgerAppHome.java
@@ -1,58 +1,54 @@
 package com.accounting.app;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Scanner;
 
 
 public class AccountingLedgerAppHome {
+    // code to show home screen, so we can navigate to other screens,
+    // Without the code below the program will not run
     public static void main(String[] args) {
+        homeScreen();
+
+
+    }
+
+    // code to prompt user to choose an option
+    //
+    public static void homeScreen() {
         Scanner scanner = new Scanner(System.in);
         String choice = "";
-
+             // here we have a while loop, when the user chooses an option the code below will run
         while (!choice.equalsIgnoreCase("X")) {
             System.out.println("Welcome to your financial transaction tracker!");
-            System.out.println("Please choose an option:");
             System.out.println("D) Add Deposit");
             System.out.println("P) Make Payment (Debit)");
             System.out.println("L) Ledger");
             System.out.println("X) Exit");
+            System.out.println("Please choose an option:");
 
             choice = scanner.nextLine();
 
+            //if the user chooses an option the code below will run
             switch (choice.toUpperCase()) {
-                case "D":
-                    addDeposit();
-                    break;
-                case "P":
-                    makePayment();
-                    break;
-                case "L":
-                    displayLedger();
-                    break;
-                case "X":
-                    System.out.println("Exiting the application. Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-                    break;
+                case "D" -> addDeposit();
+                case "P" -> makePayment();
+                case "L" -> displayLedger();
+                case "X" -> System.out.println("Exiting the application. Goodbye!");
+                default ->// if the user does not choose an option the code below will run
+                        System.out.println("Invalid choice. Please try again.");
             }
         }
     }
 
-
+    // code to prompt user for deposit information and save it to the csv file
     private static void addDeposit() {
-        // code to prompt user for deposit information and save it to the csv file
+        //Here will prompt user for deposit information
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter deposit information:");
 
-        System.out.print("Date (YYYY-MM-DD): ");
+        System.out.print("Date (YYYY-MM-DD): ");// the user will enter the date of deposit , and it will get add it to the transactions.csv file
         String date = scanner.nextLine();
 
         System.out.print("Time (HH:MM:SS): ");
@@ -66,22 +62,26 @@ public class AccountingLedgerAppHome {
 
         System.out.print("Amount: ");
 
-        BigDecimal amount = scanner.nextBigDecimal();
+        double amount = scanner.nextDouble();
 
-        String transaction = String.format("%s|%s|%s|%s|%s\n", date, time, description, vendor, amount);
+        //Here will save the deposit information to the transactions.csv file
 
         try {
             FileWriter writer = new FileWriter("transactions.csv", true);
-            writer.write(transaction);
+            //The code below will format the information into a csv file
+            writer.write("\n" + date + "|" + time + "|" + description + "|" + vendor + "|" + amount);
             writer.close();
             System.out.println("Deposit added successfully.");
         } catch (IOException e) {
-            System.out.println("Failed to add deposit. Please try again.");
+            System.out.println("Failed to add deposit. Please try again.");//When the code fails this will print the error message
         }
-
+        homeScreen();
 
     }
+
+    //the code below will make a payment
     private static void makePayment() {
+        //Here will prompt user for payment information
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter payment information:");
 
@@ -99,20 +99,22 @@ public class AccountingLedgerAppHome {
 
         System.out.print("Amount: ");
 
-       double amount = Double.parseDouble(scanner.nextLine());
+        double amount = scanner.nextDouble();
 
-        String transaction = String.format("%s|%s|%s|%s|-%s\n", date, time, description, vendor, amount);
-
+        //here will save the payment(negative value) information to the transactions.csv file
         try {
             FileWriter writer = new FileWriter("transactions.csv", true);
-            writer.write(transaction);
+            writer.write("\n" + date + "|" + time + "|" + description + "|" + vendor + "|" + "-" + amount);
             writer.close();
             System.out.println("Payment added successfully.");
         } catch (IOException e) {
             System.out.println("Failed to add payment. Please try again.");
         }
     }
+
+    //the code below will display the ledger
     private static void displayLedger() {
+
         Ledger.showLedger();
     }
 
--- src/main/java/com/accounting/app/Ledger.java
+++ src/main/java/com/accounting/app/Ledger.java
@@ -1,22 +1,67 @@
 package com.accounting.app;
 
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
+
 public class Ledger {
+    //We use a thread local to store the transactions safely
+    public static final ThreadLocal<ArrayList<Transaction>> transactions = ThreadLocal.withInitial(Ledger::getTransactions);
+
+    //We use ArrayList to reader files with thr BufferedReader to read the file from transactions.csv file
+    public static ArrayList<Transaction> getTransactions() {
+        ArrayList<Transaction> transactions = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader("transactions.csv");
+            BufferedReader bufreader = new BufferedReader(fileReader);
+
+            String input;
+            while ((input = bufreader.readLine()) != null) {
+                String[] details = input.split("\\|");//We split the string by | to get the details of each transaction
+                  //We parse the information of each transaction
+                LocalDate date = LocalDate.parse(details[0]);
+                LocalTime time = LocalTime.parse(details[1]);
+                String description = details[2];
+                String vendor = details[3];
+                double amount = Double.parseDouble(details[4]);
+
+                Transaction transaction = new Transaction(date, time, description, vendor, amount);
+                transactions.add(transaction);
+
+            } //Here where we catch any fill not found
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (IOException e) {
+            System.out.println("File not found!");
+            System.exit(0);
+        }
+
+        return transactions;
+    }
+
     public static void showLedger() {
         Scanner scanner = new Scanner(System.in);
         String choice = "";
-
+          //The code below wil show the user 5 different options,
         while (!choice.equalsIgnoreCase("X")) {
             System.out.println("Welcome to your Account Ledger!");
-            System.out.println("Please choose an option:");
             System.out.println("A) All Entries");
             System.out.println("D) Deposits");
             System.out.println("P) Payments");
             System.out.println("R) Reports");
+            System.out.println("H) Home");
+            System.out.println("Please choose an option:");
 
             choice = scanner.nextLine();
-
+                /*When the user chooses an option it goes to the another screen ,
+                to display more options.For example if the user chooses "A" it will go to the all entries screen
+                or if the user chooses "R" it will go to the reports screen with more option*/
             switch (choice.toUpperCase()) {
                 case "A":
                     allEntries();
@@ -30,6 +75,86 @@ public class Ledger {
                 case "R":
                     reports();
                     break;
+                case "H":
+                    AccountingLedgerAppHome.homeScreen();
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+
+    }
+      /* The code below will display all the information about the entries
+      from the cvs file*/
+
+    private static void allEntries() {
+        System.out.println("All Entries");
+        for (Transaction item : transactions.get()) {
+            System.out.println(item.getDate() + " " +
+                    item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+        }
+
+    }
+
+    // The code below will only display deposits entries , which is made possible by the code in line 103.
+    private static void deposits() {
+        System.out.println("Deposit Entries");
+        for (Transaction item : transactions.get()) {
+            if (item.getAmount() > 0) { //This is the line where we show only deposits
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            }
+        }
+    }
+        // The code below will only display payment entries , which is made possible by the code in line 113.
+    private static void payments() {
+        System.out.println("Payments Entries");
+        for (Transaction item : transactions.get()) {
+            if (item.getAmount() < 0) { //This is the line where we show only payments
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            }
+        }
+    }
+
+    private static void reports() {
+        Scanner scanner = new Scanner(System.in);
+        String choice = "";
+              /*Here we have the report screen.
+              The report screen will display different information for each option
+              the user chose.
+               */
+        while (!choice.equalsIgnoreCase("X")) {
+            System.out.println("Welcome to your Reports!");
+            System.out.println("1) Month To Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) Home");
+            System.out.println("Please choose an option:");
+
+            choice = scanner.nextLine();
+              /*Here where we take the user to a different screen ,
+              everytime where they choose an option, */
+            switch (choice.toUpperCase()) {
+                case "1":
+                    monthToDate();
+                    break;
+                case "2":
+                    previousMonth();
+                    break;
+                case "3":
+                    yearToDate();
+                    break;
+                case "4":
+                    previousYear();
+                    break;
+                case "5":
+                    searchByVendor();
+                    break;
+                case "0":
+                    AccountingLedgerAppHome.homeScreen();
                 default:
                     System.out.println("Invalid choice. Please try again.");
                     break;
@@ -38,17 +163,89 @@ public class Ledger {
 
     }
 
-
-
-    private static void allEntries() {
-
-    }
-    private static void deposits() {
-    }
-    private static void payments(){
+    //When the user chooses option 5, it will show the user all the vendor names.
+    private static void searchByVendor() {
+        System.out.println("The vendor name");
+        for (Transaction item : transactions.get()) {
+            System.out.println(item.getVendor());
+        }
 
     }
-    private static void reports(){
 
+
+    //
+    private static void previousYear() {
+        System.out.println("Transactions from the previous year");
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+
+        for (Transaction item : transactions.get()) {
+            LocalDate transactionDate = item.getDate();
+            if (transactionDate.getYear() == currentYear - 1) {
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            }
+        }
+    }
+
+    private static void yearToDate() {
+        System.out.println("Transactions from this year");
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+
+        for (Transaction item : transactions.get()) {
+            LocalDate transactionDate = item.getDate();
+            if (transactionDate.getYear() == currentYear) {
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            }
+        }
+    }
+
+
+    private static void previousMonth() {
+        System.out.println("Transactions from the previous month");
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+        int currentMonth = currentDate.getMonthValue();
+
+        for (Transaction item : transactions.get()) {
+            LocalDate transactionDate = item.getDate();
+            int transactionYear = transactionDate.getYear();
+            int transactionMonth = transactionDate.getMonthValue();
+
+            if (transactionYear == currentYear && transactionMonth == currentMonth - 1) {
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            } else if (transactionYear == currentYear - 1 && transactionMonth == 12 && currentMonth == 1) {
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+            }
+        }
+    }
+
+    private static void monthToDate() {
+        System.out.println("Transactions from the current month to date");
+        LocalDate currentDate = LocalDate.now();
+        int currentYear = currentDate.getYear();
+        int currentMonth = currentDate.getMonthValue();
+        int currentDay = currentDate.getDayOfMonth();
+
+        for (Transaction item : transactions.get()) {
+            LocalDate transactionDate = item.getDate();
+            int transactionYear = transactionDate.getYear();
+            int transactionMonth = transactionDate.getMonthValue();
+            int transactionDay = transactionDate.getDayOfMonth();
+
+            if (transactionYear == currentYear && transactionMonth == currentMonth && transactionDay == currentDay) {
+                System.out.println(item.getDate() + " " +
+                        item.getTime() + " " + item.getDescription() + " " + item.getVendor() + " " + item.getAmount());
+
+            }
+        }
     }
 }
+
+
+
+
